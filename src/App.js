17a64714f242a0f94de5e801856385680f971f36
sajaddmon <rahmdel.s@gmail.com>
2022-10-08 17:28:05 +0900

    Middle section finished

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
 import './App.css';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
     <div className="App">
-      {/* div center content */}
-        <div className='roomfac'>
-          {/* <Row>
-            <img src={require('./assets/sajad_rahmdel.png')} alt="sajad_rahmdel" width={266} height={266} />
-          </Row>
-          <Row>
-            <h5>Sajad Rahmdel</h5>
-          </Row> */}
-        </div>
-      {/* <Row>
-          <Col>
-            <img
-              alt='sajad rahmdel'
-              width={266}
-              height={266}
-              src=
-              {require('./assets/sajad_rahmdel.png')}
-            />
-          </Col>
-          <Col>
-            <h5>Sajad Rahmdel</h5>
-          </Col>
-          <Col>
-            <body1>Senior Mobile Application Developer</body1>
-          </Col>
-
-          <Col>
-            <body2>#Flutter &nbsp; #Android &nbsp; #iOS</body2>
-          </Col>
-          {/* Col > Blue Button with text "Download Portfolio" with pdf icon*/}
-      {/* <Col>
-          <Button variant="primary" size="lg" active>
+      {/* Middle Section */}
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={12}>
+          <img src={require('./assets/sajad_rahmdel.png')} alt="sajad_rahmdel" width={266} height={266} />
+        </Grid>
+        <Grid item xs={12}>
+          <h5>Sajad Rahmdel</h5>
+        </Grid>
+        <Grid item xs={12}>
+          <body1>Senior Mobile Application Developer</body1>
+        </Grid>
+        <Grid item xs={12}>
+          <body2>#Flutter &nbsp; #Android &nbsp; #iOS</body2>
+        </Grid>
+        <Box sx={{ m: 3 }} />
+        <Grid item xs={12}>
+          <Button variant="contained" size="lg" active>
             Download Portfolio
           </Button>
-        </Col> */}
-
-      {/* </Row> */}
-
+        </Grid>
+      </Grid>
     </div >
   );
 }
