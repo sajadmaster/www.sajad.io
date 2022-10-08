75a99451c2bc1a3008f9fea8387820aba5262675
sajaddmon <rahmdel.s@gmail.com>
2022-10-08 13:56:45 +0900

    column with sajad rahmdel, senior developer, flutter android ios finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <img
+              alt='sajad rahmdel'
+              width={266}
+              height={266}
+              src=
+              {require('./assets/sajad_rahmdel.png')}
+            />
+          </Col>
+          <Col>
+            <h5>Sajad Rahmdel</h5>
+          </Col>
+          <Col>
+            <body1>Senior Mobile Application Developer</body1>
+          </Col>
+
+          <Col>
+            <body2>#Flutter &nbsp; #Android &nbsp; #iOS</body2>
+          </Col>
+          {/* Col > Blue Button with text "Download Portfolio" with pdf icon*/}
+          <Col>
+            <Button variant="primary" size="lg" active>
+              Download Portfolio
+            </Button>
+          </Col>
+          
+        </Row>
+      </Container>
+
     </div>
   );
 }
